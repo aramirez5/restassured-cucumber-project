c8db92250639153d2857d8fc238a75cc04a536bc
ATSISTEMAS\aramirez.lorente <aramirez.lorente@atsistemas.com>
2023-05-26 15:37:31 +0200

    Restore values

--- src/test/java/steps/BraveNewCoinAPISteps.java
+++ src/test/java/steps/BraveNewCoinAPISteps.java
@@ -6,46 +6,27 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
-import java.util.Properties;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
 public class BraveNewCoinAPISteps {
 
     private static RequestSpecification request;
     private Response response;
-    private ValidatableResponse json; 
-
-    
+    private ValidatableResponse json;
 
     @Given("^I have a valid API key for the (.*) URI$")
-    public void iSetTheRequestParams(String URI){
-        
-        Properties properties = new Properties();
-
-        try {
-            FileInputStream fileInputStream = new FileInputStream("config.properties");
-            properties.load(fileInputStream);
-            fileInputStream.close();
-
-            String rapidApiKey = properties.getProperty("bravenewcoin.rapidapi.key");
-
-            request = given()
-            .header("X-RapidAPI-Key", rapidApiKey)
-            .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
-            .contentType(ContentType.JSON)
-            .baseUri(URI)
-            .log().all();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void iSetTheRequestParams(String URI) {
+        request = given()
+                .header("X-RapidAPI-Key", "bc789f3b21msh15f7d3aaaeb4d0fp18cbc6jsnbf3aa716e3fd")
+                .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
+                .contentType(ContentType.JSON)
+                .baseUri(URI)
+                .log().all();
     }
 
     @When("^I send a POST request with a valid (.*) payload to the (.*) endpoint$")
     public void sendPOSTRequest(String payload, String endpoint) {
-        File requestBody = new File("src/test/resources/payloads/"+payload+".json");
+        File requestBody = new File("src/test/resources/payloads/" + payload + ".json");
 
         response = request.when().body(requestBody).post(endpoint).prettyPeek();
     }
@@ -53,35 +34,35 @@ public class BraveNewCoinAPISteps {
     @Then("^I can validate I received a valid token in the response$")
     public void validateTheToken() {
         response = request
-        .when()
-        .post("/oauth/token");
+                .when()
+                .post("/oauth/token");
 
-        json = response.then().statusCode(200);   
+        json = response.then().statusCode(200);
     }
 
     @Given("^I have an invalid API key for the (.*) URI$")
-    public void iNotSetTheRequestParams(String URI){
+    public void iNotSetTheRequestParams(String URI) {
         request = given()
-            .header("X-RapidAPI-Key", "z673k2b458jsh946e1aeb4v981e9p12fo7bjst2bca47l349cf")
-            .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
-            .contentType(ContentType.JSON)
-            .baseUri(URI)
-            .log().all();
+                .header("X-RapidAPI-Key", "z673k2b458jsh946e1aeb4v981e9p12fo7bjst2bca47l349cf")
+                .header("X-RapidAPI-Host", "bravenewcoin.p.rapidapi.com")
+                .contentType(ContentType.JSON)
+                .baseUri(URI)
+                .log().all();
     }
 
     @Then("^I receive an HTTP code status (.*)$")
     public void validateStatus(int expectedStatusCode) {
         response = request
-        .when()
-        .post("/oauth/token");
+                .when()
+                .post("/oauth/token");
 
-        json = response.then().statusCode(expectedStatusCode);   
+        json = response.then().statusCode(expectedStatusCode);
     }
 
     @When("^I send a POST request without 'grant_type' in it's body to the (.*) endpoint$")
     public void sendWrongPOSTRequest(String endpoint) {
         File requestBody = new File("src/test/resources/payloads/WrongTokenRequestBody.json");
 
-        response = request.when().body(requestBody).post(endpoint).prettyPeek();  
+        response = request.when().body(requestBody).post(endpoint).prettyPeek();
     }
 }
